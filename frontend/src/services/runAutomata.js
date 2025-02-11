cbe19290cabdff5d3584aefee0a1d873d37aa5e0
Tony Mullen <a.mullen@northeastern.edu>
2025-02-10 20:45:47 -0800

    More fsa run animation logic

--- frontend/src/services/runAutomata.js
+++ frontend/src/services/runAutomata.js
@@ -1,35 +1,47 @@
 export function stepFSA(state, index, cy, automaton) {
     cy.nodes().forEach((n)=>{
         n.addClass('running');
+        n.removeClass('active');
     });
     cy.edges().forEach((e)=>{
         e.addClass('running');
+        e.removeClass('active');
     });
     if (state === 0) {
         cy.nodes('#start').addClass('active');
     }
+    console.log(">>"+automaton.tape.contents[index]+"<<");
 
-    let nextEdge;
-    cy.edges().forEach(e=>{
-        console.log("Source state:")
-        console.log(e.data().source);
-        console.log(state);
-        console.log("Read val")
-        console.log(e.data().read);
-        console.log(automaton.tape.contents[index]);
-        console.log("\n");
-        if (e.data().read == automaton.tape.contents[index]
-                &&
-            e.data().source == state) {
-                console.log("Match")
-                nextEdge = e
-            }
-        })
+    if (automaton.tape.contents[index] == ' ') {
+        cy.nodes().forEach((n)=>{
+            n.removeClass('running');
+        });
+        cy.edges().forEach((e)=>{
+            e.removeClass('running');
+        });
+        if (cy.nodes('#'+state).data().accept) {
+            cy.nodes('#'+state).addClass('accepting');
+        } else {
+            cy.nodes('#'+state).addClass('rejected');
+        }
 
-    cy.nodes('#'+state).addClass('active');
+        return ({"nextS": null,
+                "nextI": null});
+    } else {
+        let nextEdge;
+        cy.edges().forEach(e=>{
+            if (e.data().read == automaton.tape.contents[index]
+                    &&
+                e.data().source == state) {
+                    nextEdge = e
+                }
+            })
+        nextEdge.addClass('active');
+        cy.nodes('#'+state).addClass('active');
 
-    return ({"nextS": nextEdge.data().target,
+        return ({"nextS": nextEdge.data().target,
             "nextI": index+1});
+    }
 };
 
 export function stepPDA() {
